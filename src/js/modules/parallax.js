8b4646d63f35975949621100a962b39550b78206
Artyom Holubov <artyom.holubov@neptuness.com>
2022-04-20 19:39:07 +0300

    feat: LM-362 join beta position updated. Clipboard area updated. fix: LM-363

--- src/js/modules/parallax.js
+++ src/js/modules/parallax.js
@@ -1,38 +1,35 @@
 import AOS from 'aos';
+import $ from 'jquery';
 
 AOS.init();
 
-var planet = document.querySelector("#planet");
+const planet = document.querySelector('#planet');
 
 function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+  el.style.transform = `translate3d(${xPos}, ${yPos}px, 0)`;
 }
 
-window.addEventListener("DOMContentLoaded", scrollLoop, false);
+window.addEventListener('DOMContentLoaded', scrollLoop, false);
 
-var xScrollPosition;
-var yScrollPosition;
+let xScrollPosition;
+let yScrollPosition;
 
 function scrollLoop() {
-    xScrollPosition = window.scrollX;
-    yScrollPosition = window.scrollY;
+  xScrollPosition = window.scrollX;
+  yScrollPosition = window.scrollY;
 
-    setTranslate(0, yScrollPosition * -0.7, planet);
-    setTranslate(0, yScrollPosition * -1.2, sphere);
-    setTranslate(0, yScrollPosition * -1.5, sphere2);
+  setTranslate(0, yScrollPosition * -0.7, planet);
+  setTranslate(0, yScrollPosition * -1.2, sphere);
+  setTranslate(0, yScrollPosition * -1.5, sphere2);
 
-    // setTranslate(0, yScrollPosition * -0.2, checkers);
+  // setTranslate(0, yScrollPosition * -0.2, checkers);
 
-
-    requestAnimationFrame(scrollLoop);
+  requestAnimationFrame(scrollLoop);
 }
 
-
 $('#joinBeta_btn').on('click',
-() => {
-  $('html, body').animate({
-    scrollTop: $('.signup').offset().top
-  }, 500);
-});
-
-
+  () => {
+    $('html, body').animate({
+      scrollTop: $('.signup').offset().top + 130,
+    }, 500);
+  });
